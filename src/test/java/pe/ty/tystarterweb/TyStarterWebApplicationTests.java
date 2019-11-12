1b645d260fcb48a6da1557000ce6b7896a0105af
Nazaret Viera Ipanaque <nazaret.vieraipanaque@tcs.com>
2019-11-11 19:10:37 -0500

    feat: initial project configuration feat: add maven configuration

---
+++ src/test/java/pe/ty/tystarterweb/TyStarterWebApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.ty.tystarterweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TyStarterWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
